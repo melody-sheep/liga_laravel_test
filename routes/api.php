a0719b1d6721a612002821ad27eba938fafe7de9
SuicidezSheep <altheradrian@gmail.com>
2026-01-21 19:30:15 +0800

    updating the screenshot

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return response()->json([
-        'message' => 'Hello World! My name is Alther Adrian P. Liga',
-        'data' => $request->user()
-    ]);
+    $user = $request->user();
+    $html = '<html><body>';
+    $html .= '<h1>Hello World!</h1>';
+    $html .= '<p>- ' . ($user ? $user->name : 'Alther Adrian Liga') . '</p>';
+    $html .= '</body></html>';
+    return response($html)->header('Content-Type', 'text/html');
 });
 
